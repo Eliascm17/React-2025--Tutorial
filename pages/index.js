c0a936df07010389de6300456b5d4fdb998f8c4d
Eliascm17 <Elias.cmoreno17@gmail.com>
2020-12-21 13:22:34 -0600

    chakra theme and _document are set up

--- pages/index.js
+++ pages/index.js
@@ -1,59 +1,40 @@
 import Head from 'next/head';
-import { useAuth } from '../lib/auth';
-import styles from '../styles/Home.module.css';
+import { Button, Text, Code, Heading } from '@chakra-ui/react';
+
+import { useAuth } from '@/lib/auth';
 
 export default function Home() {
     const auth = useAuth();
     return (
-        <div className={styles.container}>
+        <div className="container">
             <Head>
-                <title>Create Next App</title>
-                <link rel="icon" href="/favicon.ico" />
+                <title>Fast Feedback</title>
             </Head>
 
-            <main className={styles.main}>
-                <h1 className={styles.title}>
-                    Welcome to <a href="https://nextjs.org">Next.js!</a>
-                </h1>
-
-                <p className={styles.description}>
-                    Get started by editing{' '}
-                    <code className={styles.code}>pages/index.js</code>
-                </p>
-                <button
-                    onClick={(e) => {
-                        auth.signinWithGithub(e);
-                    }}
-                >
-                    Sign In
-                </button>
-                <div>{auth?.user?.email}</div>
-
-                {auth?.user && (
-                    <button
+            <main>
+                <Heading>Fast Feedback</Heading>
+                <Text>
+                    Current user:
+                    <Code>{auth.user ? auth.user.email : ' None'}</Code>
+                </Text>
+                {auth.user ? (
+                    <Button
                         onClick={(e) => {
                             auth.signout(e);
                         }}
                     >
                         Sign Out
-                    </button>
+                    </Button>
+                ) : (
+                    <Button
+                        onClick={(e) => {
+                            auth.signinWithGithub(e);
+                        }}
+                    >
+                        Sign In
+                    </Button>
                 )}
             </main>
-
-            <footer className={styles.footer}>
-                <a
-                    href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Powered by{' '}
-                    <img
-                        src="/vercel.svg"
-                        alt="Vercel Logo"
-                        className={styles.logo}
-                    />
-                </a>
-            </footer>
         </div>
     );
 }
