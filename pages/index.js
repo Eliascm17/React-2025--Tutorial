d8b1facfefabf4bb6f4ab30b97ff9697a2a3b336
Elias Moreno <Elias.cmoreno17@gmail.com>
2021-01-06 11:19:14 -0600

    Dev (#3)
    * working on spotify auth
    
    * firestore works for saving user data
    
    * chakra theme and _document are set up
    
    * created a nav, dashboard, and modal
    
    * added firebase admin for api calls and added SWR
    
    * created site feedback pages
    
    * authenticated api routes, showing sites for 1 user
    
    * changed landing page, added feedback
    
    * squashed a bug when creating a new site
    
    Co-authored-by: Elias Moreno <eliasmoreno@Elias.local>

--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,110 @@
 import Head from 'next/head';
-import { Button, Text, Code, Heading } from '@chakra-ui/react';
+import { Button, Text, Link, Flex, Box } from '@chakra-ui/react';
 
 import { useAuth } from '@/lib/auth';
+import { Logo } from '@/styles/logo';
+import { Github } from '@/styles/github';
+import { Google } from '@/styles/google';
 
 export default function Home() {
     const auth = useAuth();
     return (
-        <div className="container">
+        <Flex
+            as="main"
+            direction="column"
+            align="center"
+            justify="center"
+            h="100vh"
+            maxW="400px"
+            margin="0 auto"
+        >
             <Head>
-                <title>Fast Feedback</title>
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                                if (document.cookie && document.cookie.includes('fast-feedback-auth')) {
+                                    window.location.href = "/dashboard"
+                                }
+                            `
+                    }}
+                />
             </Head>
-
-            <main>
-                <Heading>Fast Feedback</Heading>
-                <Text>
-                    Current user:
-                    <Code>{auth.user ? auth.user.email : ' None'}</Code>
-                </Text>
-                {auth.user ? (
-                    <Button
-                        onClick={(e) => {
-                            auth.signout(e);
-                        }}
+            <Logo boxSize="64px" />
+            <Flex>
+                <Text mb={4} fontSize="lg" py={4}>
+                    <Text as="span" fontWeight="bold" display="inline">
+                        Fast Feedback
+                    </Text>
+                    {' was built as part of '}
+                    <Link
+                        href="https://react2025.com"
+                        isExternal
+                        textDecoration="underline"
                     >
-                        Sign Out
-                    </Button>
-                ) : (
+                        React 2025
+                    </Link>
+                    {`. It's the easiest way to add comments or reviews to your static site. Try it out by leaving a comment below. After the comment is approved, it will display below.`}
+                </Text>
+            </Flex>
+            {auth.user ? (
+                <Button
+                    as="a"
+                    href="/dashboard"
+                    backgroundColor="white"
+                    color="gray.900"
+                    variant="outline"
+                    fontWeight="medium"
+                    mt={4}
+                    size="lg"
+                    _hover={{ bg: 'gray.100' }}
+                    _active={{
+                        bg: 'gray.100',
+                        transform: 'scale(0.95)'
+                    }}
+                >
+                    View Dashboard
+                </Button>
+            ) : (
+                <>
                     <Button
                         onClick={(e) => {
                             auth.signinWithGithub(e);
                         }}
+                        leftIcon={<Github />}
+                        backgroundColor="gray.900"
+                        color="white"
+                        fontWeight="medium"
+                        mt={4}
+                        size="lg"
+                        _hover={{ bg: 'gray.700' }}
+                        _active={{
+                            bg: 'gray.800',
+                            transform: 'scale(0.95)'
+                        }}
                     >
-                        Sign In
+                        Sign In with Github
                     </Button>
-                )}
-            </main>
-        </div>
+                    <Button
+                        onClick={(e) => {
+                            auth.signinWithGoogle(e);
+                        }}
+                        leftIcon={<Google />}
+                        backgroundColor="white"
+                        color="gray.900"
+                        variant="outline"
+                        fontWeight="medium"
+                        mt={4}
+                        size="lg"
+                        _hover={{ bg: 'gray.100' }}
+                        _active={{
+                            bg: 'gray.100',
+                            transform: 'scale(0.95)'
+                        }}
+                    >
+                        Sign In with Google
+                    </Button>
+                </>
+            )}
+        </Flex>
     );
 }
